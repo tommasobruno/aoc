8e789cc480543dc8f204526cbe9c53f9a2df504d
Tommaso Bruno <bruno.tommaso@protonmail.com>
2023-12-17 09:28:14 +0000

    feat: remove main

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,4 @@
 const std = @import("std");
 const listUtils = @import("./list.zig");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-
-    var list = try listUtils.GenericList(u32).init(allocator);
-    defer list.deinit();
-
-    try list.push(3);
-
-    std.debug.print("{any}\n", .{list.items[0..list.pos]});
-
-    _ = list.pop();
-    std.debug.print("{any}\n", .{list.items[0..list.pos]});
-}
+pub fn main() !void {}
