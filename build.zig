cd5fdd895be46b585c8c23de51035d8c33a890ef
Tommaso Bruno <bruno.tommaso@protonmail.com>
2023-12-17 05:20:46 +0000

    fix: improve build.zig

--- build.zig
+++ build.zig
@@ -57,16 +57,21 @@ pub fn build(b: *std.Build) void {
     // but does not run it.
 
     const unit_tests = b.addTest(.{
-        .root_source_file = FileSource.relative("src/main.zig"),
+        .root_source_file = FileSource.relative("src/list.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
+    const run_install_tests = b.addInstallArtifact(unit_tests, .{});
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
+    const test_unit_step = b.step("test-unit", "Run list tests");
+    test_unit_step.dependOn(&run_unit_tests.step);
+    test_unit_step.dependOn(&run_install_tests.step);
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }
